169ec3dd7d9258d5c06e37ec4777c53ee6d3d6ef
Enzo Souane <enzo.souane@epita.fr>
2022-03-30 14:22:24 +0200

    Ajout d'une class RequestHandler et POST/GET
    Traitement de l'URL avec RequestHandler en utilisant un fichier JSON
    Ajout des méthodes SEND et POST dans le class Server

--- src/main/java/fr/lernejo/navy_battle/Server2.java
+++ src/main/java/fr/lernejo/navy_battle/Server2.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 
 public class Server2 extends Server {
     private final Option<ServerInfo> localServer = new Option<>();
+    private final Option<ServerInfo> remoteServer = new Option<>();
 
     @Override
     public void startServer(int port, String connectURL) throws IOException {
@@ -22,7 +23,22 @@ public class Server2 extends Server {
     }
     @Override
     public void createContextes(HttpServer server) {
+        server.createContext("/api/game/start", s -> startGame(new RequestHandler(s)));
 
     }
+    public void startGame(RequestHandler handler) throws IOException {
+        try {
+            remoteServer.set(ServerInfo.fromJSON(handler.getJSONObject()));
+            System.out.println("Vous allez vous battre contre " + remoteServer.get().getUrl());
+
+            handler.sendJSON(202, localServer.get().toJSON());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            handler.sendString(400, e.getMessage());
+        }
+    }
+
+
 
 }
